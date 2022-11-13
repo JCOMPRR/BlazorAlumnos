71ac2c4124d63858a0677fcc84a511f93122e26f
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2022-11-12 17:02:53 -0700

    Cargando proyecto

---
+++ BlazorAlumnos/Server/Model/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorAlumnos.Server.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAlumnos.Server.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
